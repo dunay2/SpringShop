ce4953d610c58a12a39fc1c4a52b6fc7d2a480aa
dunay2 <34416258+dunay2@users.noreply.github.com>
2020-02-14 12:49:19 +0000

    Fixed shows errror message on front

--- FullStackShop/FullStackShop/src/main/java/com/shop/rest/exception/user/UserNotFoundAdvice.java
+++ FullStackShop/FullStackShop/src/main/java/com/shop/rest/exception/user/UserNotFoundAdvice.java
@@ -1,10 +1,16 @@
 package com.shop.rest.exception.user;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 
 import com.shop.rest.exception.FieldValidationError;
 import com.shop.rest.exception.FieldValidationErrorDetails;
@@ -16,15 +22,11 @@ import java.util.List;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import lombok.extern.slf4j.Slf4j;
 
 @ControllerAdvice
+@Slf4j
 class UserNotFoundAdvice {
 
 	private MessageSource messageSource;
@@ -45,7 +47,7 @@ class UserNotFoundAdvice {
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public ResponseEntity<FieldValidationErrorDetails> handleValidationError(
 			MethodArgumentNotValidException mNotValidException, HttpServletRequest request) {
-
+		 
 		FieldValidationErrorDetails fErrorDetails = new FieldValidationErrorDetails();
 		fErrorDetails.setError_timeStamp(new Date().getTime());
 		fErrorDetails.setError_status(HttpStatus.BAD_REQUEST.value());
@@ -64,6 +66,9 @@ class UserNotFoundAdvice {
 			fValidationErrorsList.add(fError);
 			fErrorDetails.getErrors().put(error.getField(), fValidationErrorsList);
 		}
+		
+		log.error("Unable to create User. Errors {} ", fErrorDetails);
+		
 		return new ResponseEntity<FieldValidationErrorDetails>(fErrorDetails, HttpStatus.BAD_REQUEST);
 	}
 
